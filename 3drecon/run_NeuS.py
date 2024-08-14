31abe58edab352fe22683432d6d8725c9174e44b
Jinhyeok <jinhyeok@unist.ac.kr>
2024-08-14 16:32:35 +0900

    .

--- 3drecon/run_NeuS.py
+++ 3drecon/run_NeuS.py
@@ -3,19 +3,21 @@ import numpy as np
 from tqdm import tqdm
 
 # ours + NeuS
-DATA_DIR = "/data2/wlsgur4011/eschernet_data/EscherNet-Dataset//GSO30"   # GSO
-exp_dir = "/home/xin/6DoF/GSO3D/"
+DATA_DIR = "/data2/wlsgur4011/eschernet_data/EscherNet-Dataset/GSO30"   # GSO
+exp_dir = "/data2/wlsgur4011/EscherNet/logs_6DoF/GSO3D"
 
 config = "configs/neus_36.yaml"
 exps = [1]
 # exps = [1, 2, 3, 5, 10]
 
 for exp in exps:
-    OUTPUT_DIR = os.path.join(exp_dir, f"logs_GSO_T{exp}M36_99k")
-    output_NeuS = f"ours_GSO_T{exp}"
+    OUTPUT_DIR = os.path.join(exp_dir, f"N{exp}M36")
+    output_NeuS = os.path.join(exp_dir, "../Neus", f"ours_GSO_T{exp}")
     os.makedirs(output_NeuS, exist_ok=True)
     obj_names = os.listdir(DATA_DIR)
     for obj_name in tqdm(obj_names):
+        if obj_name == "readme.txt":
+            continue
         if os.path.exists(os.path.join(output_NeuS, "NeuS", obj_name, "mesh.ply")):
             print("NeuS already trained for: ", obj_name)
             continue
@@ -23,12 +25,10 @@ for exp in exps:
         os.system(f"rm -rf {output_NeuS}/NeuS/{obj_name}")
         print("Training NeuS for: ", obj_name)
         input_img = os.path.join(OUTPUT_DIR, obj_name, "0.png")
-        # input_img = os.path.join(OUTPUT_DIR, obj_name, "gt.png")    #  ground truth image
+        # input_img = ∫os.path.join(OUTPUT_DIR, obj_name, "gt.png")    #  ground truth image
         cmd = f"python train_renderer.py -i {input_img} \
               -d {DATA_DIR} \
               -n {obj_name} \
               -b {config} \
               -l {output_NeuS}/NeuS"
         os.system(cmd)
-        break
-    break
