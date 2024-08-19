caad1275d918066fb47202b29aaf64a56a328dac
Jinhyeok <jinhyeok@unist.ac.kr>
2024-08-19 20:12:15 +0900

    .

--- notebooks/make_small_data.py
+++ notebooks/make_small_data.py
@@ -23,50 +23,50 @@ root_dir = "/data2/wlsgur4011/zero123_data/views_release/"
 new_dir = root_dir.replace("zero123_data", "zero123_data_small")
 
 # 1. copy npz files
-os.system(f"rsync -avm --include='*.npy' --include='*/' --exclude='*' {root_dir}/ {new_dir}/")
+# os.system(f"rsync -avm --include='*.npy' --include='*/' --exclude='*' {root_dir}/ {new_dir}/")
 
 # 2. prepocess images
-train_dataset = ObjaverseData(root_dir=root_dir,
-                              image_transforms=image_transforms,
-                              validation=False,
-                              T_in=0,
-                              T_out=1,
-                              save_preprocessed=True,
-                              use_preprocessed=False,
-                              )
+for is_validation in [True, False]:
+    train_dataset = ObjaverseData(root_dir=root_dir,
+                                  image_transforms=image_transforms,
+                                  validation=is_validation,
+                                  T_in=0,
+                                  T_out=1,
+                                  save_preprocessed=True,
+                                  use_preprocessed=False,
+                                  )
 
-train_dataloader = torch.utils.data.DataLoader(
-    train_dataset,
-    shuffle=False,
-    batch_size=16,
-    num_workers=16,
-)
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        shuffle=False,
+        batch_size=16,
+        num_workers=16,
+    )
 
-for data in tqdm(train_dataloader):
-    pass
+    for data in tqdm(train_dataloader):
+        pass
 
+    # 3. check new_dataset working
+    new_dataset = ObjaverseData(root_dir=new_dir,
+                                image_transforms=image_transforms,
+                                validation=is_validation,
+                                T_in=1,
+                                T_out=1,
+                                fix_sample=True,
+                                use_preprocessed=True,
+                                )
+    old_dataset = ObjaverseData(root_dir=root_dir,
+                                image_transforms=image_transforms,
+                                validation=is_validation,
+                                T_in=1,
+                                T_out=1,
+                                fix_sample=True,
+                                use_preprocessed=False,
+                                )
 
-# 3. check new_dataset working
-new_dataset = ObjaverseData(root_dir=new_dir,
-                            image_transforms=image_transforms,
-                            validation=False,
-                            T_in=1,
-                            T_out=1,
-                            fix_sample=True,
-                            use_preprocessed=True,
-                            )
-old_dataset = ObjaverseData(root_dir=root_dir,
-                            image_transforms=image_transforms,
-                            validation=False,
-                            T_in=1,
-                            T_out=1,
-                            fix_sample=True,
-                            use_preprocessed=False,
-                            )
-
-for idx in range(100):
-    new_data = new_dataset[idx]["image_input"]
-    old_data = old_dataset[idx]["image_input"]
-    if not torch.all(new_data == old_data):
-        raise ValueError("new_dataset not working")
-print("new_dataset working")
+    for idx in range(100):
+        new_data = new_dataset[idx]["image_input"]
+        old_data = old_dataset[idx]["image_input"]
+        if not torch.all(new_data == old_data):
+            raise ValueError("new_dataset not working")
+    print("new_dataset working")
